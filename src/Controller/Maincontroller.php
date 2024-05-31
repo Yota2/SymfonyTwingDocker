c57518c242cb93d7fcebcdecb9d3038dc6ab8b51
Your Name <you@example.com>
2024-05-31 03:16:44 +0200

    firstpushe

---
+++ src/Controller/Maincontroller.php
@@ -0,0 +1,13 @@
+<?php
+namespace  App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+#[Route('/')]
+public function homepage(){
+return new Response('<strong>Startshop</strong>: This is the shop where Every body likes to shop!');
+}
+}
